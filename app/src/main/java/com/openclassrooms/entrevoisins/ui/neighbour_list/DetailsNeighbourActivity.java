8caaed2e4f4ea6a1703c2a12ed39a4b74d3e318c
Njeudji <Njeudji@protonmail.com>
2021-05-15 11:29:54 +0200

    restart project

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailsNeighbourActivity.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+
+public class DetailsNeighbourActivity extends AppCompatActivity {
+    private NeighbourApiService mApiService;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_neighbour);
+        mApiService = DI.getNeighbourApiService();
+    }
+}
